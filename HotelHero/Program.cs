3fa946e34a1d612c7a0669e1afe26cc5be8f6bb7
agata olczak-peuker <pojker@gmail.com>
2024-04-08 15:10:43 +0200

    Rename project and solution to Hotel Hero

--- HotelHero/Program.cs
+++ HotelHero/Program.cs
@@ -3,8 +3,8 @@
     internal class Program
     {
         static void Main(string[] args)
-        //{
-        //    Console.WriteLine("Hello, World!");
-        //}
+        {
+            Console.WriteLine("Hello, World!");
+        }
     }
 }
