7a50029fb6096197e3d94466289f04c388684545
Angelixi <angelixix@gmail.com>
2024-04-12 18:51:02 +0200

    Add Menu

--- HotelHero/Program.cs
+++ HotelHero/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace MasterBooking
+﻿using System.ComponentModel.Design;
+
+namespace HotelHero
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            UserMenu.Menu();
         }
     }
 }
---
+++ HotelHero/UserMenu.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace HotelHero
+{
+    internal class UserMenu
+    {
+        public static void Menu()
+        {
+            Console.WriteLine("Select an item from the list");
+            Console.WriteLine("1.Language selection");
+            Console.WriteLine("2.Currency selection");
+            Console.WriteLine("3.User registration");
+            Console.WriteLine("4.User login");
+            Console.WriteLine("5.Offer search");
+            Console.WriteLine("6.Customer panel");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    LanguageSelection();
+                    break;
+                case 2:
+                    CurrencySelection();
+                    break;
+                case 3:
+                    UserRegistration();
+                    break;
+                case 4:
+                    UserLogin();
+                    break;
+                case 5:
+                    OfferSearch();
+                    break;
+                case 6:
+                    CustomerPanel();
+                    break;
+            }
+        }
+        static void LanguageSelection()
+        {
+            Console.WriteLine("You are in Language selection");
+            Console.WriteLine("Choose language");
+            Console.WriteLine("1.Polski");
+            Console.WriteLine("2.English");
+            Console.WriteLine("3.Español");
+
+            int language = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            switch (language)
+            {
+                case 1:
+                    Console.WriteLine("Wybrałeś język Polski");
+                    break;
+                case 2:
+                    Console.WriteLine("You chose English");
+                    break;
+                case 3:
+                    Console.WriteLine("Has elegido español");
+                    break;
+
+            }
+            Menu();
+        }
+        static void CurrencySelection()
+        {
+            Console.WriteLine("Select currency");
+            Console.WriteLine("1.PLN");
+            Console.WriteLine("2.EUR");
+            Console.WriteLine("3.GBP");
+            int currency = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            switch (currency)
+            {
+                case 1:
+                    Console.WriteLine("Ustawiono PLN");
+                    break;
+                case 2:
+                    Console.WriteLine("Set EUR");
+                    break;
+                case 3:
+                    Console.WriteLine("Set GBP");
+                    break;
+            }
+            Menu();
+        }
+
+        static void UserRegistration()
+        {
+            Console.WriteLine("Tu powstanie kreatywna rejestracja");
+            Console.WriteLine();
+            Menu();
+        }
+        static void UserLogin()
+        {
+            Console.WriteLine("Tu powstanie zwariowane logowanie");
+            Console.WriteLine();
+            Menu();
+        }
+        static void OfferSearch()
+        {
+            Console.WriteLine("Tu powstanie zakręcone wyszukiwanie");
+            Console.WriteLine();
+            Menu();
+        }
+
+        static void CustomerPanel()
+        {
+            Console.WriteLine("Padłeś? Powstań! Czas na wakacje ;p");
+            Console.WriteLine();
+            Menu();
+        }
+    }
+}
