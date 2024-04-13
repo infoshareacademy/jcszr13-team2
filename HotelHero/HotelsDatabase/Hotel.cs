9c1f00962e75df1731b9c62532e95403ddd9b831
Kamil Drzyzga <kamildrzyzga94@gmail.com>
2024-04-13 20:42:26 +0200

    add hotels repositiory

---
+++ HotelHero/HotelsDatabase/Hotel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelHero.HotelsDatabase
+{
+    internal class Hotel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Description { get; set; }
+        public int Stars { get; set; }
+        public float Rating { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Hotel: {Name} \nLiczba gwiazdek: {numberOfStars(Stars)} \nAdres: {Address}, {City} \nOpis: {Description} \nOcena: {Rating}";
+        }
+
+        private string numberOfStars(int number)
+        {
+            string stars = null;
+            for (int i = 0; i < number; i++)
+                stars += "*";
+            return stars;
+        }
+    }
+}
---
+++ HotelHero/HotelsDatabase/HotelsRepository.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelHero.HotelsDatabase
+{
+    internal class HotelsRepository
+    {
+        public List<Hotel> Repository {  get; set; }  
+
+        public HotelsRepository()
+        {
+            initialisation();
+        }
+
+        private void initialisation()
+        {
+            Repository = new List<Hotel>()
+            {
+                new Hotel(){ Name = "Hotel 1",Address = "Słowackiego 1", City = "Warszawa", Description = "Hotel w Warszawie", Stars = 4, Rating = 4.5f },
+                new Hotel(){ Name = "Hotel 2",Address = "Sienkiewicza 3", City = "Warszawa", Description = "Najlepszy hotel w Warszawie", Stars = 5, Rating = 5.0f },
+                new Hotel(){ Name = "Hotel 3",Address = "Mickiewicza 45", City = "Kraków", Description = "Hotel w Krakowie", Stars = 3, Rating = 3.7f },
+                new Hotel(){ Name = "Hotel 4",Address = "Fredry 36", City = "Poznań", Description = "Hotel w Poznaniu", Stars = 5, Rating = 4.7f },
+            };
+
+        }
+    }
+}
