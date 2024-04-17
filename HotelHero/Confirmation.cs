90e0af8967c9b7491d2556169ed710b28c501e4d
agata olczak-peuker <pojker@gmail.com>
2024-04-17 21:16:22 +0200

    delete Room, add Reservation, Confirmation

---
+++ HotelHero/Confirmation.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+
+namespace MasterBooking
+{
+    internal class Confirmation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Do you confirm selected offer?");
+            Console.WriteLine($"Press 'Y' to proceed with your reservation");
+            Console.WriteLine($"Press backspace to return to offer browser");
+            var option = Console.ReadKey();
+
+            if (option.Key != ConsoleKey.Backspace && option.Key != ConsoleKey.Y)
+                {
+                    Console.WriteLine("Plese press 'Y' or backspace");
+                }
+
+            if (option.Key == ConsoleKey.Backspace)
+                {
+                Console.WriteLine($"Search for your destination");
+                }
+
+            else if (option.Key == ConsoleKey.Y)
+                {
+                Console.WriteLine($"Please select your payment method");
+                Console.WriteLine($"Press:");
+                Console.WriteLine($"1 - for credit or debit card");
+                Console.WriteLine($"2 - for bank transfer");
+                }
+            var payment = Console.ReadKey();
+            
+        }
+    }
+}
---
+++ HotelHero/Reservation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelHero
+{
+    public class Reservation
+    {
+        public int ReservationNumber { get; set; }
+        public string UserEmail { get; set; }
+        public int Price { get; set; }
+
+        public Reservation(int reservationnumber, string useremail, int price)
+        {
+            ReservationNumber = reservationnumber;
+            UserEmail = useremail;
+            Price = price;
+        }
+
+
+
+
+
+    }
+}
