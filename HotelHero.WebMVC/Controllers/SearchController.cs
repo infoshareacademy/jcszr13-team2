54a712e88221319ea266757c6a759c246446bd77
Rafał Daszczuk <rafal.daszczuk@gmail.com>
2024-07-04 22:08:23 +0200

    Search button added to the main page

---
+++ HotelHero.WebMVC/Controllers/SearchController.cs
@@ -0,0 +1,29 @@
+﻿using HotelHero.WebMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace HotelHero.WebMVC.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly SearchPanel _searchService;
+
+        public SearchController()
+        {
+            _searchService = new SearchPanel();
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Results(string query)
+        {
+            List<string> results = _searchService.Search(query);
+            return View(results);
+        }
+    }
+}
---
+++ HotelHero.WebMVC/Services/SearchPanel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace HotelHero.WebMVC.Services
+{
+    public class SearchPanel
+    {
+        public List<string> Search(string query)
+        {
+            List<string> results = new List<string>();
+            return results;
+        }
+    }
+}
